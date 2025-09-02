738db6467da8c721991ee6c69728521c32a9c969
MahmoudHousam <mahmoudhousam60@gmail.com>
2025-09-03 02:07:02 +0300

    change t main.py

--- backend/main.py
+++ backend/main.py
@@ -124,3 +124,8 @@ async def predict(request: dict):
             "message": str(e),
             "status": "error"
         }, 500
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
